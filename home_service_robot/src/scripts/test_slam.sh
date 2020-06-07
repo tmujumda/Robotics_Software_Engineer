c562a7e23c24a4b1f2cfdefba884884df1b9eca1
tmujumda <tmujumda@umich.edu>
2020-06-07 12:08:31 -0700

    slam launch file moved to my_robot package, teleop command updated

--- home_service_robot/src/scripts/test_slam.sh
+++ home_service_robot/src/scripts/test_slam.sh
@@ -8,7 +8,7 @@ xterm -e "cd "$workspace_path" && source devel/setup.bash && roslaunch my_robot 
 sleep 5
 
 # Open the workspace, source and launch gmapping.launch
-xterm -e "cd "$workspace_path" && source devel/setup.bash && roslaunch gmapping slam_gmapping_tapan.launch" &
+xterm -e "cd "$workspace_path" && source devel/setup.bash && roslaunch my_robot slam_gmapping_tapan.launch" &
 
 sleep 5
 
@@ -18,4 +18,4 @@ xterm -e "cd "$workspace_path" && source devel/setup.bash && rosrun rviz rviz" &
 sleep 5
 
 # Open the workspace, source and launch keyboard_teleop.launch
-xterm -e "cd "$workspace_path" && source devel/setup.bash && roslaunch teleop_twist_keyboard teleop.launch"
+xterm -e "cd "$workspace_path" && source devel/setup.bash && rosrun teleop_twist_keyboard teleop_twist_keyboard.py"
